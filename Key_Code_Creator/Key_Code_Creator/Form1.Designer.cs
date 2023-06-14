058e857fd6cdf4c936ba67afd82407964c9b104b
Mehmet Batuhan Demir <53302194+ottoman81@users.noreply.github.com>
2023-06-15 01:08:14 +0300

    Add files via upload

---
+++ Key_Code_Creator/Key_Code_Creator/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Key_Code_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.KeytextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DItextBox = new System.Windows.Forms.TextBox();
+            this.LngtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // KeytextBox
+            // 
+            this.KeytextBox.Location = new System.Drawing.Point(12, 189);
+            this.KeytextBox.Name = "KeytextBox";
+            this.KeytextBox.Size = new System.Drawing.Size(333, 20);
+            this.KeytextBox.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 244);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 277);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(333, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start/Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 527);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Export to Text Document";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 527);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Export to Excell";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Key Code";
+            // 
+            // DItextBox
+            // 
+            this.DItextBox.Location = new System.Drawing.Point(12, 130);
+            this.DItextBox.Name = "DItextBox";
+            this.DItextBox.Size = new System.Drawing.Size(252, 20);
+            this.DItextBox.TabIndex = 7;
+            // 
+            // LngtextBox
+            // 
+            this.LngtextBox.Location = new System.Drawing.Point(12, 59);
+            this.LngtextBox.Name = "LngtextBox";
+            this.LngtextBox.Size = new System.Drawing.Size(252, 20);
+            this.LngtextBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Dash Interval";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Length";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 555);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LngtextBox);
+            this.Controls.Add(this.DItextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.KeytextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox KeytextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DItextBox;
+        private System.Windows.Forms.TextBox LngtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Key_Code_Creator/Key_Code_Creator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Key_Code_Creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            GenerateRandomString();
+        }
+        public  void GenerateRandomString()
+        {
+            try
+            {
+                KeytextBox.Text = "";
+                
+                int length = Convert.ToInt32(LngtextBox.Text); // Dize uzunluğu
+                int dashInterval = Convert.ToInt32(DItextBox.Text); // Tire eklenen karakter aralığı
+                string characters = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"; // Kullanılacak karakterler
+
+                StringBuilder stringBuilder = new StringBuilder();
+                Random random = new Random();
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (i > 0 && i % dashInterval == 0)
+                    {
+                        stringBuilder.Append("-");
+                    }
+
+                    int randomIndex = random.Next(0, characters.Length);
+                    char randomChar = characters[randomIndex];
+                    stringBuilder.Append(randomChar);
+                }
+                KeytextBox.Text = stringBuilder.ToString();
+                listBox1.Items.Add(KeytextBox.Text);
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GenerateRandomString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+
+            {
+                timer1.Enabled = true;
+                LngtextBox.Enabled = false;
+                DItextBox.Enabled=false;
+                KeytextBox.Enabled = false;
+                button1.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+
+
+
+
+
+
+            }
+            else if (timer1.Enabled == true)
+
+            {
+                timer1.Enabled = false;
+                LngtextBox.Enabled = true;
+                DItextBox.Enabled = true;
+                KeytextBox.Enabled = true;
+                button1.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+
+
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string dosyaAdi =listBox1.Items.Count +" Key Codes of "+ LngtextBox.Text +" X "+ DItextBox.Text + ".txt";
+            using (StreamWriter writer = new StreamWriter(dosyaAdi))
+            {
+                foreach (string item in listBox1.Items)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+    }
+}
